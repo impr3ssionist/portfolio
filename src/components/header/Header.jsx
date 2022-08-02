74bd6b8ae37aa34b8ba43603e7339d5c06c10f94
impr3ssionist <87039516+impr3ssionist@users.noreply.github.com>
2022-08-02 10:28:10 -0500

    added CSS variables and built out the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello! I'm</h5>
+        <h1>Caleb Ford</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+      </div>
+      </header>
+    
+   
   )
 }
 
