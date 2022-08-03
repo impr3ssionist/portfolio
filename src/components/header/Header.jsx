7b192973b5df6068c0d49c20e696cc2e87acee8f
impr3ssionist <87039516+impr3ssionist@users.noreply.github.com>
2022-08-03 13:03:21 -0500

    styled the header component and began work on the nav component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
+import ME from '../../assets/Circle_Headshot.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,13 @@ const Header = () => {
         <h1>Caleb Ford</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA/>
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
       </header>
     
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsFacebook} from 'react-icons/bs'
+
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        
+        <a href="https://www.linkedin.com/in/caleb-ford13/" target='_blank'><BsLinkedin /></a>
+        <a href="https://github.com/impr3ssionist" target='_blank'><BsGithub/></a>
+        <a href="https://www.facebook.com/caleb.p.ford.1" target='_blank'><BsFacebook/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
