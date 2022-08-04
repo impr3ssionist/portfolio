11cd8daf5ff8f2dbdc33f3ae45bd76fa8f2153c6
impr3ssionist <87039516+impr3ssionist@users.noreply.github.com>
2022-08-04 17:28:28 -0500

    Built out and styled the nav and the portfolio components

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/Circle_Headshot.png'
+import {FaAward} from 'react-icons/fa'
+import {AiFillFolderOpen} from 'react-icons/ai'
 
 const about = () => {
   return (
-    <div>about</div>
-  )
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className='about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image!" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>FullStack Academy Graduate</small>
+            </article>
+
+            <article className='about__card'>
+              <AiFillFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+
+            
+       
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod nulla, vitae porro libero nesciunt reiciendis non amet deserunt aut quasi reprehenderit eligendi magni minima explicabo hic laudantium quis ea est?</p>
+          <a href="'#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+      
+      </section>  )
 }
 
 export default about
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './Experience.css'
+import {BsCheck2All } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id='experience'>
+      <h5>Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+
+            <article className='experience__details'>
+              <BsCheck2All className='experience__details-icon'/>
+             <div> <h4>HTML</h4>
+              <small className='text-light'>Experienced</small></div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheck2All className='experience__details-icon'/>
+              <div><h4>CSS</h4>
+              <small className='text-light'>Experienced</small></div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheck2All className='experience__details-icon'/>
+              <div><h4>JavaScript</h4>
+              <small className='text-light'>Intermediate</small></div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheck2All className='experience__details-icon'/>
+              <div><h4>React</h4>
+              <small className='text-light'>Intermediate</small></div>
+            </article>
+
+            
+          </div>
+        </div>
+
+        {/* ************** End of Frontend ******************** */}
+        
+        <div className="experience__backend">
+                    <h3>Backend</h3>
+                      <article className='experience__details'>
+              <BsCheck2All className='experience__details-icon'/>
+              <div><h4>Node.js</h4>
+              <small className='text-light'>Experienced</small></div>
+            </article>
+
+            <article className='experience__details'>
+              <BsCheck2All className='experience__details-icon'/>
+              <div><h4>postgreSQL</h4>
+              <small className='text-light'>Familiar</small></div>
+            </article>
+
+           
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
