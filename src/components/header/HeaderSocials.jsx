ecb2dc050e6b4bb06bd1b6b3ea2153377a9b611b
Caleb Ford <cpford13@gmail.com>
2024-08-22 14:05:02 -0600

    add rel=noopener noreferrer to links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,13 +6,25 @@ import { BsFacebook } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://www.linkedin.com/in/caleb-ford13/" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/caleb-ford13/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <BsLinkedin />
       </a>
-      <a href="https://github.com/impr3ssionist" target="_blank">
+      <a
+        href="https://github.com/impr3ssionist"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <BsGithub />
       </a>
-      <a href="https://www.facebook.com/caleb.p.ford.1" target="_blank">
+      <a
+        href="https://www.facebook.com/caleb.p.ford.1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <BsFacebook />
       </a>
     </div>
